ed2cebc909a9bdb70035a7f5684d123baf1825c2
Dhaval Solanki <dhavalas24@gmail.com>
2023-08-22 09:52:41 +0530

    Cpp program

---
+++ hello1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+   int a , b , c ;
+
+   cin >> a >> b >> c;
+
+   cout << "A Value is " << a <<"\n"; 
+   cout << "B Value is " << b <<"\n"; 
+   cout << "C Value is " << c <<"\n"; 
+}
