bd8791b8e03bce800db8af12d31d9ac37177c7d9
Dhaval Solanki <dhavalas24@gmail.com>
2023-09-02 14:56:30 +0530

    2 Sep

---
+++ destructor_class.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+class Area
+{
+    public :
+
+    Area(double r)
+    {
+        cout <<"Area Of Circle is. : "<< 3.14 * r * r;
+    }
+
+    Area(int s)
+    {
+        cout <<"\nArea Of Squre is. : "<< s * s;
+    }
+
+    Area(double w, double h)
+    {
+        cout <<"\nArea of Rectrangle is. : "<< w * h;
+    }
+    
+    ~ Area()
+    {
+        cout <<"\nDestructor Distroy ";
+    }
+    Area(int b, int h)
+    {
+        cout <<"\nArea of Triangle is. : "<< (b * h) / 2;
+    }
+
+};
+int main()
+{
+    Area a(20.56),b(15),c(10.99,5.23),d(),e(23,16);
+}
