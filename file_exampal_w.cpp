1bc1bb5b9c4f9e4796ffc5f7a92ca2f41eac7723
Dhaval Solanki <dhavalas24@gmail.com>
2023-09-27 15:15:07 +0530

    27 Sep

---
+++ file_exampal_w.cpp
@@ -0,0 +1,29 @@
+// file managment
+// ofstream : stream class to write on file
+// ifstream : stream class to read from file
+// ffstream : stream class to both read and write from / to file
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream filestream("D:/ds_cpp/ds_cpp/file1.txt");
+    //ofstream filestream("file1.txt");
+    if (filestream.is_open())
+    {
+        filestream << "My Name is Dhaval";
+        filestream << "\nI Go Skill Qode Daily";
+        filestream << "\nAnd I learn the full stack in SkillQode";
+        filestream.clear();
+    }
+
+    else
+    {
+        cout << "File Opening Is Fail";
+    }
+    
+    return 0;
+}
